9daf3e8a68ec396ae1860c8d12ba6a437d43cd95
Omar Baldi <omarbaldidk@gmail.com>
2022-10-13 18:12:00 +0200

    Installed faker js to get random sentence on component mount + used dynamic import for boost performance + applied style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 import { useTextSelection } from './hooks/useTextSelection';
+import './App.css';
 
 function App() {
-  const textSelected = useTextSelection();
+  const numberLines = useRef<number>(3);
+  const [sentence, setSentence] = useState<string>('');
+  const textSelected: string = useTextSelection();
+
+  const generateRandomWord = async (): Promise<string> => {
+    const { faker } = await import('@faker-js/faker');
+
+    const maxNumberLines = numberLines.current;
+    const randomWord = faker.lorem.lines(maxNumberLines);
+
+    return randomWord;
+  };
+
+  useEffect(() => {
+    generateRandomWord().then(setSentence);
+  }, []);
 
   return (
     <div className='App'>
-      <h1>Tomorrow</h1>
-      {textSelected}
+      <div className='container'>
+        <div className='textContainer'>
+          <h1 className='sentence'>{sentence}</h1>
+          <h3 className='textSelected'>{textSelected}</h3>
+        </div>
+      </div>
     </div>
   );
 }
